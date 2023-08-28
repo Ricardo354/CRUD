74a95b13356227d5c0530ed5ca6e6f79e28be1f8
Ricardo354 <ricardotmd@protonmail.com>
2023-08-27 21:39:53 -0300

    Adding some comments so i can better understand the code

--- app.py
+++ app.py
@@ -11,8 +11,7 @@ class Todo(db.Model):
     content = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def __repr__(self):
-        return '<Task %r>' % self.id.data
+        
 @app.route('/', methods=['POST', 'GET'])
 
 def index():
@@ -20,7 +19,7 @@ def index():
     if request.method == 'POST':
         task_content = request.form['content'] #JUST THE INPUT
         new_task = Todo(content=task_content) # NEW_TASK HAS ID, CONTENT AND DATE_CREATED
-        if new_task.content == '':
+        if new_task.content == '': #NULLABLE DIDNT WORK, HAD TO DO THIS
             return 'You cant add a NULL value'
         try:
             db.session.add(new_task)
@@ -33,20 +32,20 @@ def index():
         return render_template('index.html', tasks=tasks)
 
     
-@app.route('/delete/<int:id>')
+@app.route('/delete/<int:id>') #CREATING DELETE ROUTE
 
 def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
+    task_to_delete = Todo.query.get_or_404(id) 
 
     try:
         db.session.delete(task_to_delete)
         db.session.commit()
-        return redirect('/')
+        return redirect('/') #ALWAYS GOTTA GO BACK HOME WHEN U FINISH SHIT LIKE THAT
     
     except:
         return 'There was a problem deleting your task'
 
-@app.route('/update/<int:id>', methods=['GET','POST'])
+@app.route('/update/<int:id>', methods=['GET','POST']) #UPDATE ROUTE
 
 def update(id):
     task = Todo.query.get_or_404(id)
